17c382158aedeab919faf7329f409b9b237c4a95
Ranagol <andorhorvat@gmail.com>
2023-10-01 10:36:29 +0200

    Small corrections.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
+        /**
+         * Route::has('login') returns a boolean value, true or false. It will be true if there is 
+         * a named route named 'login' defined in your Laravel application's route configuration 
+         * and false if it doesn't exist.
+         */
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -25,11 +30,20 @@ Route::get('/', function () {
 });
 
 Route::middleware([
-    'auth:sanctum',
+    /**
+     * This is an array of middleware names that will be applied to the routes within the group.
+     */
+    'auth:sanctum',//checks if the user is authenticated using Laravel Sanctum. 
     config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+    'verified',// checks if the user's email address has been verified
+])->group(function () {//This code sets up a group of routes that share authentication middleware
     Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Dashboard');//it's using the Inertia.js library to render a 'Dashboard' component.
     })->name('dashboard');
+});
+
+
+Route::get('photos', function () {
+    //dd(Photo::all());
+    return Inertia::render('Guest/Photos');
 });
